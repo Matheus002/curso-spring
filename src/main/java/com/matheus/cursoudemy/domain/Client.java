56add37b024e0feee01f2938539504c003069720
Matheus <matheus002_@hotmail.com>
2021-09-16 17:11:47 -0300

    feat: add Client and Address models

---
+++ src/main/java/com/matheus/cursoudemy/domain/Client.java
@@ -0,0 +1,116 @@
+package com.matheus.cursoudemy.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import com.matheus.cursoudemy.domain.enums.ClientType;
+
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String email;
+	private String cpfOrCnpj;
+	private Integer type;
+	
+	private List<Address> addresses = new ArrayList<>();
+	
+	private Set<String> phones = new HashSet<>();
+	
+	public Client() {
+		
+	}
+
+	public Client(Integer id, String name, String email, String cpfOrCnpj, ClientType type) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.cpfOrCnpj = cpfOrCnpj;
+		this.type = type.getCod();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOrCnpj() {
+		return cpfOrCnpj;
+	}
+
+	public void setCpfOrCnpj(String cpfOrCnpj) {
+		this.cpfOrCnpj = cpfOrCnpj;
+	}
+
+	public ClientType getType() {
+		return ClientType.toEnum(type);
+	}
+
+	public void setType(ClientType type) {
+		this.type = type.getCod();
+	}
+
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+
+	public Set<String> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(Set<String> phones) {
+		this.phones = phones;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+	
+}
