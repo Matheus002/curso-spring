6a6f4fdee2c7622c4b94cde911f637c1ab7218b4
Matheus <matheus002_@hotmail.com>
2021-09-16 10:38:35 -0300

    feat: add h2 database, data jpa and devtools

--- src/main/java/com/matheus/cursoudemy/resources/CategoryResource.java
+++ src/main/java/com/matheus/cursoudemy/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.matheus.cursoudemy.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.matheus.cursoudemy.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@GetMapping
-	public String list() {
-		return "REST está funcionando!";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 
 }
