77855696f783d683e317fb8ee12d0167d6d10444
Matheus <matheus002_@hotmail.com>
2021-10-19 09:27:03 -0300

    feat: add CategoryDTO

--- src/main/java/com/matheus/cursoudemy/resources/CategoryResource.java
+++ src/main/java/com/matheus/cursoudemy/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.matheus.cursoudemy.resources;
 
 import java.net.URI;
+import java.util.List;import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheus.cursoudemy.domain.Category;
+import com.matheus.cursoudemy.dto.CategoryDTO;
 import com.matheus.cursoudemy.services.CategoryService;
 
 @RestController
@@ -27,8 +30,7 @@ public class CategoryResource {
 	@GetMapping("/{id}")
 	public ResponseEntity<Category> find(@PathVariable Integer id) {
 		Category obj = service.find(id);
-		return ResponseEntity.ok().body(obj);
-			
+		return ResponseEntity.ok().body(obj);			
 	}
 	
 	@PostMapping
@@ -50,6 +52,13 @@ public class CategoryResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping()
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<Category> list = service.findAll();
+		List<CategoryDTO> listDto = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);			
 	}
 
 }
--- src/main/java/com/matheus/cursoudemy/services/CategoryService.java
+++ src/main/java/com/matheus/cursoudemy/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.matheus.cursoudemy.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoryService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Unable to delete the selected Category because it has references on others tables");
 		}
+	}
+	
+	public List<Category> findAll() {
+		return repo.findAll();
 	}
 
 }
