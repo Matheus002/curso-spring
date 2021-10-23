bc759885456ff3cd00bd95c676d3e77f765c7b8e
matheus002 <matheus002_@hotmail.com>
2021-10-22 23:07:27 -0300

    feat: Add CRUD operations for Client Model

--- src/main/java/com/matheus/cursoudemy/services/ClientService.java
+++ src/main/java/com/matheus/cursoudemy/services/ClientService.java
@@ -1,12 +1,19 @@
 package com.matheus.cursoudemy.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.matheus.cursoudemy.domain.Client;
+import com.matheus.cursoudemy.dto.ClientDTO;
 import com.matheus.cursoudemy.repositories.ClientRepository;
+import com.matheus.cursoudemy.services.exceptions.DataIntegrityException;
 
 @Service
 public class ClientService {
@@ -19,5 +26,39 @@ public class ClientService {
 		return obj.orElseThrow(() -> new com.matheus.cursoudemy.services.exceptions.ObjectNotFoundException(
 				"Object not found! Id: " + id + ", Type: " + Client.class.getName()));
 	}
+	
+	public Client insert(Client obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Client update(Client obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete( Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Unable to delete the selected Client because it has references on others tables");
+		}
+	}
+	
+	public List<Client> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Client fromDto(ClientDTO oobjDto) {
+		throw new UnsupportedOperationException();		
+	}
+
 
 }
