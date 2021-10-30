657e8f89fe25aedaeb7d7bd67d23a047f8314a34
matheus002 <matheus002_@hotmail.com>
2021-10-30 18:27:54 -0300

    chore: adjust email validation

--- src/main/java/com/matheus/cursoudemy/services/validation/InsertClientValidator.java
+++ src/main/java/com/matheus/cursoudemy/services/validation/InsertClientValidator.java
@@ -5,13 +5,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.matheus.cursoudemy.domain.Client;
 import com.matheus.cursoudemy.domain.enums.ClientType;
 import com.matheus.cursoudemy.dto.NewClientDTO;
+import com.matheus.cursoudemy.repositories.ClientRepository;
 import com.matheus.cursoudemy.resources.exception.FieldMessage;
 import com.matheus.cursoudemy.services.validation.utils.BR;
 
 
 public class InsertClientValidator implements ConstraintValidator<InsertClient, NewClientDTO> {
+
+ @Autowired
+ private ClientRepository repo;
+	
  @Override
  public void initialize(InsertClient ann) {
  }
@@ -25,6 +33,11 @@ public class InsertClientValidator implements ConstraintValidator<InsertClient, 
 	 
 	 if (objDto.getType().equals(ClientType.LEGALPERSON.getCod()) && !BR.isValidCNPJ(objDto.getCpfOrCnpj())) {
 		 list.add(new FieldMessage("cpfOrCnpj", "Invalid CNPJ"));
+	 }
+	 
+	 Client aux = repo.findByEmail(objDto.getEmail());	 
+	 if (aux != null) {
+		 list.add(new FieldMessage("email", "Email already in use"));
 	 }
  
 	 for (FieldMessage e : list) {
