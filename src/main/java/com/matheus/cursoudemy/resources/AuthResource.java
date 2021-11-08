bc65a3d86ddce5d619c80dbeff257e637f210f14
matheus002 <matheus002_@hotmail.com>
2021-11-08 06:37:49 -0300

    feat: add refresh token

--- src/main/java/com/matheus/cursoudemy/resources/AuthResource.java
+++ src/main/java/com/matheus/cursoudemy/resources/AuthResource.java
@@ -1,5 +1,30 @@
 package com.matheus.cursoudemy.resources;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.matheus.cursoudemy.security.JWTUtil;
+import com.matheus.cursoudemy.security.UserSS;
+import com.matheus.cursoudemy.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
 public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@PostMapping(value="/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+	UserSS user = UserService.authenticated();
+	String token = jwtUtil.generateToken(user.getUsername());
+	response.addHeader("Authorization", "Bearer " + token);
+	return ResponseEntity.noContent().build();
+	}
 
 }
